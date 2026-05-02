8d9ea94ecc31594e732e593de8c33770df37ef60
RazanAl-Quran <quraan_razan@hotmail.com>
2026-05-03 01:56:55 +0300

    feat: added BST part-02

--- 08-BST/src/BST.java
+++ 08-BST/src/BST.java
@@ -23,5 +23,41 @@ public class BST {
         }
         return root;
     }
+
+    // Method to search for a value in the BST
+    public boolean search(int val) {
+        return searchNode(this.root, val);
+    }
+
+    // Helper method to recursively search for a value in the BST
+    private boolean searchNode(TreeNode root, int val) {
+        if (root == null) {
+            return false;
+        }
+
+        if (val == root.val) {
+            return true;
+        } else if (val < root.val) {
+            return searchNode(root.left, val);
+        } else {
+            return searchNode(root.right, val);
+        }
+    }
+
+    // Method to perform inorder traversal of the BST
+    public void inorderTraversal() {
+        inorder(this.root);
+        System.out.println();
+    }
+
+    // Helper method to recursively perform inorder traversal
+    private void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.val + " ");
+            inorder(root.right);
+        }
+    }
+
     
 }
